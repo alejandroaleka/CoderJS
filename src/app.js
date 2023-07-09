c4dc8ce954a899d5cfa9b261c7cacb1ee8be4ecc
Alejandro Fernandez <alejandro.aleka@gmail.com>
2023-07-09 12:50:16 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -9,20 +9,6 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* app.use("/static", express.static("public"));
-
-app.use((req, res, next) => {
-  console.log("Middleware a nivel aplicación");
-
-  return next();
-});
-
-app.use((err, req, res, next) => {
-  console.log("Middleware para manejo de errores");
-
-  return next();
-}); */
-
 app.use("/api/products", productsRouter);
 
 app.use("/api/carts", cartRouter);
