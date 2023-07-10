fc7b49b50901b317c4bd279f2634a27d1ed25671
Alejandro Fernandez <alejandro.aleka@gmail.com>
2023-07-09 22:37:20 -0300

    updates

--- src/routers/cartRouter.js
+++ src/routers/cartRouter.js
@@ -2,12 +2,27 @@ const { Router } = require("express");
 
 const cartRouter = Router();
 
-const cart = [];
+const carts = [];
 
 cartRouter.post("/", async (req, res) => {
-  const prod = req.body;
+  const cart = {
+    id: cart.products.length,
+    products: [],
+  };
 
-  return res.status(201).json(prod);
+  carts.push(cart);
+
+  if (req.body) {
+    res.status(500).send({ error: "uso incorrecto" });
+  }
+  return res.status(201).json(carts);
 });
+
+cartRouter.get("/:cid", async (req, res) => {
+  const cid = parseInt(req.params.cid, 10);
+  const cart = carts.map(cid === carts.id);
+});
+
+cartRouter.post("/:cid/product/:pid", async (req, res) => {});
 
 module.exports = cartRouter;
